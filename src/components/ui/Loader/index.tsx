341b5c086f98dd51ae539866dc061784e47651d5
Vadym Kononenko <70842045+Vadymk95@users.noreply.github.com>
2024-11-02 20:39:05 +0200

    implemented loader

--- src/components/ui/Loader/index.tsx
+++ src/components/ui/Loader/index.tsx
@@ -1,5 +1,12 @@
 import { FC } from 'react';
+import './index.css';
 
 export const Loader: FC = () => {
-    return <div>Loading...</div>;
+    return (
+        <div className="loader-container">
+            <div className="loader">
+                <div className="loader-circle"></div>
+            </div>
+        </div>
+    );
 };
