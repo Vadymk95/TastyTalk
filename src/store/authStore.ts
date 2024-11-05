979f841e5aa9bf7926b5b3bbe54805cae5aa71b8
Vadym Kononenko <70842045+Vadymk95@users.noreply.github.com>
2024-11-05 16:00:02 +0200

    added register method

--- src/store/authStore.ts
+++ src/store/authStore.ts
@@ -1,10 +1,12 @@
-import { auth, googleProvider } from '@root/firebase/firebaseConfig';
+import { auth, db, googleProvider } from '@root/firebase/firebaseConfig';
 import {
+    createUserWithEmailAndPassword,
     signInWithEmailAndPassword,
     signInWithPopup,
     signOut,
     User
 } from 'firebase/auth';
+import { doc, setDoc } from 'firebase/firestore';
 import { create } from 'zustand';
 
 interface AuthState {
@@ -14,6 +16,12 @@ interface AuthState {
     signInWithEmail: (email: string, password: string) => Promise<void>;
     signInWithGoogle: () => Promise<void>;
     signOutUser: () => Promise<void>;
+    registerWithEmailAndProfile: (
+        email: string,
+        password: string,
+        firstName: string,
+        lastName: string
+    ) => Promise<void>;
     setLoading: (value: boolean) => void;
     setError: (error: string | null) => void;
     clearError: () => void;
@@ -63,6 +71,37 @@ export const useAuthStore = create<AuthState>((set) => ({
         try {
             await signOut(auth);
             set({ user: null, error: null });
+        } catch (error: any) {
+            set({ error: error.message });
+        } finally {
+            set({ loading: false });
+        }
+    },
+
+    registerWithEmailAndProfile: async (
+        email,
+        password,
+        firstName,
+        lastName
+    ) => {
+        set({ loading: true });
+        try {
+            set({ error: null });
+            const userCredential = await createUserWithEmailAndPassword(
+                auth,
+                email,
+                password
+            );
+            const user = userCredential.user;
+
+            await setDoc(doc(db, 'users', user.uid), {
+                email: user.email,
+                firstName,
+                lastName,
+                createdAt: new Date()
+            });
+
+            set({ user, error: null });
         } catch (error: any) {
             set({ error: error.message });
         } finally {
