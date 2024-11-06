cecc002e0f3fbedee0e26aa62676fef60bc2b39a
Vadym Kononenko <70842045+Vadymk95@users.noreply.github.com>
2024-11-06 15:03:45 +0200

    added hidden header after scrolling

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react';
+import { FC, useCallback, useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
 
@@ -7,9 +7,33 @@ import { routes } from '@root/router/routes';
 
 export const Header: FC = () => {
     const { t } = useTranslation();
+    const [isVisible, setIsVisible] = useState(true);
+    const [lastScrollY, setLastScrollY] = useState(0);
+
+    const handleScroll = useCallback(() => {
+        const currentScrollY = window.scrollY;
+
+        if (currentScrollY > lastScrollY && currentScrollY > 100) {
+            setIsVisible(true);
+        } else {
+            setIsVisible(false);
+        }
+
+        setLastScrollY(currentScrollY);
+    }, [lastScrollY]);
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, [handleScroll]);
 
     return (
-        <header className="header">
+        <header
+            className={`header duration-500 ${isVisible ? 'transform translate-y-0' : 'transform -translate-y-full'}`}
+        >
             <div className="container flex justify-between">
                 <Link to={routes.home} className="link-primary nav-link p-4">
                     {t('Header.brand')}
