19a15ba68a0f0fabf887b1e4b8daf979525f6a34
Vadym Kononenko <70842045+Vadymk95@users.noreply.github.com>
2024-11-11 13:32:34 +0200

    improved styles

--- src/components/ui/GreetingCard/index.tsx
+++ src/components/ui/GreetingCard/index.tsx
@@ -10,7 +10,7 @@ export const GreetingCard: FC<GreetingCardProps> = ({ to, title }) => {
     return (
         <Link
             to={to}
-            className="bg-neutral-100 p-8 rounded-lg shadow-custom-light hover:shadow-lg hover:bg-neutral transition duration-300 text-center justify-center flex flex-col items-center"
+            className="bg-neutral-100 p-10 sm:p-6 rounded-lg shadow-custom-light hover:shadow-lg hover:bg-neutral transition duration-300 text-center justify-center flex flex-col items-center"
         >
             <p className="text-xl font-semibold text-primary mb-2">{title}</p>
             <span className="text-neutral-dark">{title.toLowerCase()}</span>
