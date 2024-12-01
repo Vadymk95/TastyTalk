ed9d0c87e84b72d11cd1ae1d4173320bbc083ba4
Vadym Kononenko <70842045+Vadymk95@users.noreply.github.com>
2024-12-01 22:26:05 +0200

    added comments

--- src/components/forms/CreateRecipeManuallyForm/Steps/getAllSteps.tsx
+++ src/components/forms/CreateRecipeManuallyForm/Steps/getAllSteps.tsx
@@ -28,54 +28,63 @@ export const GetAllSteps = (
 
     const steps = [
         {
+            // Title and required fields
             id: 1,
             title: t('Stepper.Steps.Recipe.1.title'),
             content: <Step1 formik={formik} />,
             isOptional: stepFieldMapping[0].isOptional
         },
         {
+            // Description
             id: 2,
             title: t('Stepper.Steps.Recipe.2.title'),
             content: <Step2 />,
             isOptional: stepFieldMapping[1].isOptional
         },
         {
+            // PhotoPreview
             id: 3,
             title: t('Stepper.Steps.Recipe.3.title'),
             content: <Step3 />,
             isOptional: stepFieldMapping[2].isOptional
         },
         {
+            // Ingredients
             id: 4,
             title: t('Stepper.Steps.Recipe.4.title'),
             content: <Step4 maxSteps={30} formik={formik} />,
             isOptional: stepFieldMapping[3].isOptional
         },
         {
+            // Steps
             id: 5,
             title: t('Stepper.Steps.Recipe.5.title'),
             content: <Step5 maxSteps={20} formik={formik} />,
             isOptional: stepFieldMapping[4].isOptional
         },
         {
+            // Tips
             id: 6,
             title: t('Stepper.Steps.Recipe.6.title'),
             content: <Step6 maxSteps={10} formik={formik} />,
             isOptional: stepFieldMapping[5].isOptional
         },
         {
+            // Warnings
             id: 7,
             title: t('Stepper.Steps.Recipe.7.title'),
             content: <Step7 maxSteps={10} formik={formik} />,
             isOptional: stepFieldMapping[6].isOptional
         },
         {
+            // Url
             id: 8,
             title: t('Stepper.Steps.Recipe.8.title'),
             content: <Step8 />,
             isOptional: stepFieldMapping[7].isOptional
         },
         {
+            // Confirmation
             id: 9,
             title: t('Stepper.Steps.Recipe.9.title'),
             content: (
