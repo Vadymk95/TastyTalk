f3051ed881d511d472695d1a676a5c0c651e3952
Vadym Kononenko <70842045+Vadymk95@users.noreply.github.com>
2024-12-04 15:28:03 +0200

    added some styles to the welcome modal

--- src/components/modals/StepperWelcomeModal/index.tsx
+++ src/components/modals/StepperWelcomeModal/index.tsx
@@ -24,10 +24,10 @@ export const StepperWelcomeModal: FC = () => {
                 <p>{t('Modals.StepperWelcomeModal.description')}</p>
 
                 <div>
-                    <h4 className="text-lg font-medium text-secondary">
+                    <h4 className="text-lg font-medium text-secondary mb-1">
                         {t('Modals.StepperWelcomeModal.stepsTitle')}
                     </h4>
-                    <ul className="list-disc pl-6">
+                    <ul className="list-disc pl-6 flex flex-col gap-2">
                         <li>{t('Stepper.Steps.Recipe.1.description')}</li>
                         <li>{t('Stepper.Steps.Recipe.2.description')}</li>
                         <li>{t('Stepper.Steps.Recipe.3.description')}</li>
