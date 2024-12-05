4604d1cdcc7f6e16bafa591a81e11dbe2d9cfa8c
Vadym Kononenko <70842045+Vadymk95@users.noreply.github.com>
2024-12-05 22:22:37 +0200

    fixed profileName problem

--- src/components/forms/EditProfileForm/index.tsx
+++ src/components/forms/EditProfileForm/index.tsx
@@ -294,7 +294,7 @@ export const EditProfileForm: FC = () => {
                                         onClick={() =>
                                             arrayHelpers.push({
                                                 name: '',
-                                                url: ''
+                                                profileName: ''
                                             })
                                         }
                                         className="mt-2"
