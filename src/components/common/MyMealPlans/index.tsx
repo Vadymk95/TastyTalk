8aceb070f58f10c7130a1547a538aea03d38bfb3
Vadym Kononenko <70842045+Vadymk95@users.noreply.github.com>
2024-12-06 17:52:04 +0200

    removed tags

--- src/components/common/MyMealPlans/index.tsx
+++ src/components/common/MyMealPlans/index.tsx
@@ -4,8 +4,6 @@ import { FC } from 'react';
 export const MyMealPlans: FC = () => {
     return (
         <section>
-            <h1>My Meal Plans</h1>
-
             <FilterBlock />
             <Grid />
         </section>
--- src/components/common/MyRecipes/index.tsx
+++ src/components/common/MyRecipes/index.tsx
@@ -5,8 +5,6 @@ import { FilterBlock, Grid } from '@root/components/common';
 export const MyRecipes: FC = () => {
     return (
         <section>
-            <h1>My Recipes</h1>
-
             <FilterBlock />
             <Grid />
         </section>
