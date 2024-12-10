119518b6ae066cab54b0ba7d47fb6d811860f017
Vadym Kononenko <70842045+Vadymk95@users.noreply.github.com>
2024-12-10 15:12:08 +0200

    fixed profile photo styles

--- src/components/common/ProfilePhoto/index.tsx
+++ src/components/common/ProfilePhoto/index.tsx
@@ -2,9 +2,10 @@ import { FC } from 'react';
 import { useTranslation } from 'react-i18next';
 
 import { Image } from '@root/components/ui';
+import { isMobileDevice } from '@root/helpers';
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 interface ProfilePhotoProps {
     profileImage: string | File | null;
@@ -12,6 +13,8 @@ interface ProfilePhotoProps {
 
 export const ProfilePhoto: FC<ProfilePhotoProps> = ({ profileImage }) => {
     const { t } = useTranslation();
+    const isMobile = isMobileDevice();
+
     return (
         <div className="w-36 h-36 sm:w-20 sm:h-20 overflow-hidden rounded-full">
             {profileImage ? (
@@ -26,8 +29,11 @@ export const ProfilePhoto: FC<ProfilePhotoProps> = ({ profileImage }) => {
                 />
             ) : (
                 <div className="bg-neutral-light text-neutral-300 w-full h-full flex flex-col gap-2 items-center justify-center">
-                    <FontAwesomeIcon size="3x" icon={faUser} />
-                    <span>{t('General.noImage')}</span>
+                    <FontAwesomeIcon
+                        size={isMobile ? '2x' : '3x'}
+                        icon={faUser}
+                    />
+                    <span className="sm:text-xs">{t('General.noImage')}</span>
                 </div>
             )}
         </div>
