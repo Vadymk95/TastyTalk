c2f68e8b9bea0b8c5dae8f2b12ef232baeab18db
Vadym Kononenko <70842045+Vadymk95@users.noreply.github.com>
2024-12-11 22:03:32 +0200

    fixed displaying of home link

--- src/components/common/HeaderNavigation/index.tsx
+++ src/components/common/HeaderNavigation/index.tsx
@@ -1,6 +1,6 @@
 import { FC } from 'react';
 import { useTranslation } from 'react-i18next';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 import { routes } from '@root/router/routes';
 import { useAuthStore } from '@root/store/authStore';
@@ -11,15 +11,19 @@ import { faHouse } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export const HeaderNavigation: FC = () => {
+    const location = useLocation();
     const { user, isEmailVerified } = useAuthStore();
     const { t } = useTranslation();
     const isAuth = !!user;
+    const isHomePath =
+        location.pathname === routes.home ||
+        location.pathname === routes.recipes;
 
     return (
         <nav className="flex">
             {isAuth ? <PrivateNavigation /> : <PublicNavigation />}
 
-            {!isEmailVerified && (
+            {!isEmailVerified && !isHomePath && (
                 <Link
                     to={routes.home}
                     className="link-primary nav-link p-4 sm:px-6"
