4598072a26b595d2baa2e3d3e173da15e5df8093
Vadym Kononenko <70842045+Vadymk95@users.noreply.github.com>
2025-03-18 20:39:53 +0200

    feat(recipes-visibility): decrease gap between recipe elements

--- src/components/ui/SectionList/index.tsx
+++ src/components/ui/SectionList/index.tsx
@@ -9,7 +9,7 @@ interface IProps {
 
 export const SectionList: FC<IProps> = ({ list }) => {
     return (
-        <ul className="grid gap-4 grid-cols-[repeat(auto-fill,_minmax(200px,_1fr))]">
+        <ul className="grid gap-2 grid-cols-[repeat(auto-fill,_minmax(200px,_1fr))]">
             {list.map((item) => (
                 <SectionRecipeItem key={item.id} recipe={item} />
             ))}
