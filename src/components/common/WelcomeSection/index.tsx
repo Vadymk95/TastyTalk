5971fa4ef06529281bdc3526b4bfe02fa6f78370
Vadym Kononenko <70842045+Vadymk95@users.noreply.github.com>
2025-03-19 12:45:40 +0200

    feat(recipes-visibility): add screen height with calculation

--- src/components/common/WelcomeSection/index.tsx
+++ src/components/common/WelcomeSection/index.tsx
@@ -11,7 +11,7 @@ export const WelcomeSection: FC = () => {
     const { t } = useTranslation();
 
     return (
-        <section className="flex flex-col items-center justify-center h-full py-8">
+        <section className="flex flex-col items-center justify-center h-[calc(100vh-112px)] py-8">
             <Image src={logo} className="w-1/2 mb-4" alt="TastyTalks" />
 
             <h1 className="main-heading text-7xl md:text-4xl">
@@ -27,7 +27,7 @@ export const WelcomeSection: FC = () => {
 
             <Link
                 to={routes.recipesCreate}
-                className="link-primary text-2xl nav-link rounded-lg px-6 py-4 bg-primary flex-all-center mt-4"
+                className="link-primary text-2xl nav-link rounded-lg px-6 py-4 bg-primary flex-all-center my-8"
             >
                 <span>{t('WelcomeSection.createRecipe')}</span>
             </Link>
